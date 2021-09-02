4e50ca73f2edde89180aafceb02472cc81bdeb79
Salmaan <salmaan.arif.ali@gmail.com>
2021-09-02 03:54:30 -0400

    commit tree d2999381e31cfd75566df3d0067ff9c9194d898a

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,34 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'AK12QD90gEcvz423dtEL2ywZtmdjIlW6';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then( resp => resp.json() )
+      .then( reviewsData => this.setState({ reviews: reviewsData.results}))
+      .catch( err => console.log(err) )
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews = {this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'AK12QD90gEcvz423dtEL2ywZtmdjIlW6';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    }
+  }
+
+  handleSubmit = (e) => {
+    this.setState({
+      searchTerm: e.target.searchTerm.value,
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Search: </label>
+          <input name="searchTerm"></input>
+          <button type="submit">Submit</button>
+        </form>
+        <MovieReviews />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
